aef371bbe7bfaabb214bcd068e66d3c9e093f3bc
Vishnu Prabhakar <vishnuprabhakar05@gmail.com>
2018-09-25 21:42:48 +0530

    Modified

--- Git_test/src/com/utility/GetIpAddr.java
+++ Git_test/src/com/utility/GetIpAddr.java
@@ -3,19 +3,20 @@ import java.net.InetAddress;
 import java.net.UnknownHostException;
 
 public class GetIpAddr {
-	public static void main(String[] args) {
+	public String getIpAddr(){
+		
+		String ipAddr = "";
 		
 		try{
+			ipAddr = InetAddress.getLocalHost().getHostAddress();
 			
-			String ipAddr = InetAddress.getLocalHost().getHostAddress();
-			System.out.println(ipAddr);
-		
-		}catch(UnknownHostException e){
-			System.out.println(e);
-		}catch (Exception ae) {
-			System.out.println(ae);
+		}catch(UnknownHostException he){
+			System.out.println(he);
 		}
-		
-		
+		catch(Exception e){
+			System.out.println(e);
+		}
+		return ipAddr;
 	}
+
 }
